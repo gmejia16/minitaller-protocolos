15fe8744f3aac723b8301ecda4d9bc23b31a16c1
gmejia16 <112442770+gmejia16@users.noreply.github.com>
2024-09-25 01:55:39 -0600

    Update server.py
    comentarios

--- HTTP/server.py
+++ HTTP/server.py
@@ -1,6 +1,6 @@
 #Se importan las clases necesarias para crear el servidor (httpserver) y manejar las solicitudes (simplehttprequesthandler)
 from http.server import HTTPServer, SimpleHTTPRequestHandler    
-import time
+import time #Este módulo es para trabajar con la fecha y hora actual
 
 #Definición de host y port
 HOST = "SU DIRECCIÓN IP"
@@ -8,21 +8,22 @@ PORT = "SU PUERTO" #TENGA EN CUENTA QUE EL PUERTO ES UN ENTERO NO UN STRING, POR
 
 
 class MiHTTP(SimpleHTTPRequestHandler):
-
+#El método do_GET maneja las soliccitudes GET 
     def do_GET(self):
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        self.send_response(200) #Envía un código de estado HTTP 200, que indica que la solicitud fue exitosa.
+        self.send_header("Content-type", "text/html") #Indica que el contenido de la respuesta será de tipo HTML.
+        self.end_headers() #Finaliza el encabezado HTTP.
 
-        self.wfile.write(bytes("<html><body><h1>HELLO WORLD!</h1</body></html>","utf-8"))
+        self.wfile.write(bytes("<html><body><h1>HELLO WORLD!</h1</body></html>","utf-8")) #nvía al cliente un mensaje HTML
 
+    #El metodo do_POST manera las solicitudes post
     def do_POST(self):
         self.send_response(200)
-        self.send_header("Content-type", "application/json")
+        self.send_header("Content-type", "application/json") #Establece que la respuesta será en formato JSON.
         self.end_headers()
 
-        date = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
+        date = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time())) #Envía al cliente un JSON con la fecha y hora actual
         self.wfile.write(bytes('{"time": "' + date + '"}', "utf-8"))
 
 
